d6a4ea9794016bf3c347a83f0ff72658d1359068
Masoud <karimi.masoud5494@gmail.com>
2022-01-21 20:41:26 +0330

    added CORS and COMPRESSION plugins

--- src/main/kotlin/info/masoudkarimi/tasky/Application.kt
+++ src/main/kotlin/info/masoudkarimi/tasky/Application.kt
@@ -1,5 +1,6 @@
 package info.masoudkarimi.tasky
 
+import info.masoudkarimi.tasky.plugins.configureHTTP
 import info.masoudkarimi.tasky.plugins.configureMonitoring
 import info.masoudkarimi.tasky.plugins.configureRouting
 import info.masoudkarimi.tasky.plugins.configureSerialization
@@ -9,6 +10,8 @@ import io.ktor.server.netty.*
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module(testing: Boolean = false) {
+    log.info("Hello from application module!")
+    configureHTTP()
     configureRouting()
     configureMonitoring()
     configureSerialization()
---
+++ src/main/kotlin/info/masoudkarimi/tasky/plugins/HTTP.kt
@@ -0,0 +1,26 @@
+package info.masoudkarimi.tasky.plugins
+
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.application.*
+
+fun Application.configureHTTP() {
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        allowCredentials = true
+    }
+
+}
